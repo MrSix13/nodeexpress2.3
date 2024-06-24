395a95b115ff672fc5d5252d9eac9ecb42da8f31
mrSix13 <bastiangarcesga@gmail.com>
2024-06-23 21:13:26 -0400

    add: new changes

--- index.js
+++ index.js
@@ -12,43 +12,19 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-async function reconnect() {
-    try {
-      await client.initialize();
-      console.log('Reconnected to WhatsApp Web!');
-    } catch (error) {
-      console.error('Error during reconnection:', error);
-      // Implement exponential backoff or retry logic here
-    }
-  }
+// async function reconnect() {
+//     try {
+//       await client.initialize();
+//       console.log('Reconnected to WhatsApp Web!');
+//     } catch (error) {
+//       console.error('Error during reconnection:', error);
+//       // Implement exponential backoff or retry logic here
+//     }
+//   }
 
-
-
-const client = new Client({
-    authStrategy: new LocalAuth({ dataPath: 'session' }),
-    webVersionCache: {
-      type: "remote",
-      remotePath:
-        "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
-    },
-  });
 
 
   
-
-client.on('qr', (qr)=>{
-    qrcode.generate(qr, {small:true})
-    console.log('QR RECEIVED', qr)
-});
-
-client.on('remote_session_saved', () => {
-    console.log('remote_session_saved')
-});
-client.on('ready', ()=>{
-    console.log('Conectado a wsp');
-});
-client.on('message', (message)=>{
-});
 
 app.get('/', (req,res)=>{
     return res.json({mensaje: "hola mundo"})
@@ -56,9 +32,38 @@ app.get('/', (req,res)=>{
 
 app.post('/enviar-mensaje', async(req,res)=>{
     const {numero, mensaje} = req.body;
-   console.log(numero)
-   console.log(mensaje)
-    try {
+
+    const client =  new Client({
+        authStrategy: new LocalAuth({ dataPath: 'session' }),
+        webVersionCache: {
+        type: "remote",
+        remotePath:
+            "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+        },
+    });
+  
+        client.on('qr', (qr)=>{
+            qrcode.generate(qr, {small:true})
+            console.log('QR RECEIVED', qr)
+        });
+
+        // client.on('remote_session_saved', () => {
+        //     console.log('remote_session_saved')
+        // });
+        client.on('ready', ()=>{
+            console.log('Conectado a wsp');
+        });
+        // client.on('message', (message)=>{
+        // });
+
+
+    await client.initialize();  
+    
+    
+//    console.log(numero)
+//    console.log(mensaje)
+ 
+   try {
         for (const phoneNumber of numero) {
             const formattedNumber = phoneNumber + '@c.us';
             console.log('formattedNumber:', formattedNumber)
@@ -67,8 +72,7 @@ app.post('/enviar-mensaje', async(req,res)=>{
        res.json({ mensaje: 'Mensajes enviados correctamente.' });
    } catch (error) {
     console.error('Error sending message:', error);
-        res.status(500).json({ mensaje: 'Error al enviar mensajes' });
-        await reconnect();
+    res.status(500).json({ mensaje: 'Error al enviar mensajes' });
     }
     
 });
@@ -76,6 +80,3 @@ app.post('/enviar-mensaje', async(req,res)=>{
 app.listen(port, ()=>{
     console.log(`servidor escuchando en el puerto ${port}`)
 });
-
-await client.initialize();   
-
